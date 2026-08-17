a3063cee21c29db0233bfa61add1dc96b4720025
Jeverly432 <nomoconov.sasha@yandex.ru>
2026-08-17 17:16:46 +0900

    feat: banner component

--- frontend/app/(site)/_ui/Banner.tsx
+++ frontend/app/(site)/_ui/Banner.tsx
@@ -1,10 +1,18 @@
+import { Button } from '@/shared';
+import banner from '@shared/assets/images/Home/banner.webp';
+import Image from 'next/image';
+
 export const Banner = () => {
   return (
     <div className='pt-10 '>
       <div className='container'>
         <div className='flex flex-col gap-5'>
           <div className='flex justify-between items-center'>
-            <h1>Единство веры. Сила Братства!</h1>
+            <h1 className='text-[46px] font-balkara'>Единство веры. Сила Братства!</h1>
+            <Button size='large'>Подробнее о Братстве</Button>
+          </div>
+          <div className='relative flex w-full h-139.5 rounded-xl overflow-hidden'>
+            <Image src={banner} alt='Единство веры. Сила Братства' fill className='object-cover' loading='eager' />
           </div>
         </div>
       </div>
--- frontend/app/(site)/page.tsx
+++ frontend/app/(site)/page.tsx
@@ -1,3 +1,9 @@
+import { Banner } from './_ui/Banner';
+
 export default function AppPage() {
-  return <div>app</div>;
+  return (
+    <div className='flex flex-col gap-20'>
+      <Banner />
+    </div>
+  );
 }
